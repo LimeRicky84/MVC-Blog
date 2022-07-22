c88de1aca599af355cfa745c9d6e1c3785cd6a38
Ricma <ric.madse@gmail.com>
2022-07-22 00:52:36 -0600

    models done, and some backbone code

---
+++ models/index.js
@@ -0,0 +1,30 @@
+const Article = require('./Article');
+const Comment = require('./Comment');
+const User = require('./User');
+
+
+User.hasMany(Article, {
+    foreignKey: 'user_id'
+})
+
+Article.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Article, {
+    foreignKey: 'article_id'
+})
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
+
+Article.hasMany(Comment, {
+    foreignKey: 'article_id'
+})
+
+module.exports = {Article, Comment, User}
---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+// Redirect to login page if user is not logged in
+
+const withAuth = (req, res, next) => {
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
