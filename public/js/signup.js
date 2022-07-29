ff26cd8fda7265377e29cc89d17481d6a3accd7e
Ricma <ric.madse@gmail.com>
2022-07-28 18:37:22 -0600

    signup form, front end js started

---
+++ public/js/signup.js
@@ -0,0 +1,16 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault()
+
+    const userName = document.querySelector('#username-signup').value.trim()
+    const email = document.querySelector('#email-signup').value.trim()
+    const password = document.querySelector('#password-signup').value.trim()
+
+    if (userName && email && password) {
+        const response = await fetch(`/api/projects`, {
+            method: 'POST',
+            body: JSON.stringify({ userName, email, password }),
+            headers: { 'content-Type': 'application/json'}
+        })
+        if (response.ok)
+    }
+}
--- seeds/articleData.js
+++ seeds/articleData.js
@@ -4,6 +4,7 @@ const articleData = [
     {
         title: "If The Romans Had This One Piece Of Tech, We'd Have Already Been To Mars",
         article_description: "Go ahead, accuse me of clickbait. I totally deserve it.",
+        date_created: 1612951424000,
         article_content: "You people will click on anything, won't you?",
     },
 ];
